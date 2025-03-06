c7dcc43468bdedb5a11efccbcbfaa9753a37a4c7
Bigyajeet Kumar Patra <141954821+Bigyajeet@users.noreply.github.com>
2025-03-06 22:36:31 +0530

    Time: 44 ms (79.98%), Space: 53.8 MB (14.67%) - LeetHub

---
+++ 2727-is-object-empty/2727-is-object-empty.js
@@ -0,0 +1,11 @@
+/**
+ * @param {object|array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    if (Array.isArray(obj)) {
+        return obj.length === 0;
+    } else {
+        return Object.keys(obj).length === 0;
+    }
+};
