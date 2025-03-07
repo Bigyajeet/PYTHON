dac6d2ae46df1d682aebcd53b54e1d7664fb4af1
Bigyajeet Kumar Patra <141954821+Bigyajeet@users.noreply.github.com>
2025-03-07 22:23:49 +0530

    Time: 39 ms (95.52%), Space: 56 MB (17.76%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    if (arr.length === 0) {
+        return [];
+    }
+    
+    const chunkedArr = [];
+    for (let i = 0; i < arr.length; i += size) {
+        chunkedArr.push(arr.slice(i, i + size));
+    }
+    return chunkedArr;
+};
